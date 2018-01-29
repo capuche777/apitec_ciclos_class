4f6b9b6cedb12db8f39503cddb067097604e57a3
Jeremias Enriquez <capuche777@gmail.com>
2018-01-29 14:44:54 -0600

    segundo ejemplo

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-var contador;
+/*var contador;
 for (var i = 1; i< 10; i++){
     contador = i*3;
 }
 
-console.log(contador);
+console.log(contador);*/
+/*-----------------------*/
+
+/*Cuando se utilizan cilos anidados se deben usar dos variables
+diferentes para cada ciclo*/
+
+var contador;
+
+for (var i = 0; i<10; i++) {
+    for (var j = 0; j<i; i++) {
+
+    }
+}
+
+/*por ejemplo en un tablero, i puede recorrer filas y j puede recorrer
+columnas*/
+
